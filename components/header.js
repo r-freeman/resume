70cb821ef181660af1ad3337e35c6483d75237d4
Ryan Freeman <N00173767@student.iadt.ie>
2021-10-19 13:52:28 +0100

    Updated text style in header

--- components/header.js
+++ components/header.js
@@ -24,8 +24,8 @@ export default function Header({author, links}) {
                             className="u-photo rounded-full"/>
                     </div>
                     <div>
-                        <h1 className="p-name text-4xl font-serif font-bold md:text-5xl leading-tight">{author.name}</h1>
-                        <p className="p-job-title font-serif text-2xl text-gray-500">{author.jobTitle}</p>
+                        <h1 className="p-name text-4xl leading-tight font-serif font-bold md:text-5xl md:leading-tight">{author.name}</h1>
+                        <p className="p-job-title font-serif text-[18px] md:text-2xl text-gray-500">{author.jobTitle}</p>
                     </div>
                 </div>
                 <div className="mt-6 font-light text-base lg:text-sm space-y-1">
