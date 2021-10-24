932d7ae9f227cb5762a59b3d5ead6c603809f2ed
Ryan Freeman <N00173767@student.iadt.ie>
2021-10-24 14:26:09 +0100

    Added external link to projects

--- components/projects.js
+++ components/projects.js
@@ -6,7 +6,7 @@ export default function Projects({projectData}) {
                 <section className="project mb-12"
                          key={project.id}>
                     <h3 className="mt-2 font-serif font-bold text-[21px] leading-none">
-                        {project.name}
+                        <a href={project.href} rel="noopener" target="_blank">{project.name}</a>
                     </h3>
                     <div className="markdown"
                          dangerouslySetInnerHTML={{__html: project.contentHtml}}/>
