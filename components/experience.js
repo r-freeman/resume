da3e6811099794e106a99012b5c2282836480a7a
Ryan Freeman <N00173767@student.iadt.ie>
2021-11-03 21:36:24 +0000

    Updated print styling

--- components/experience.js
+++ components/experience.js
@@ -7,13 +7,13 @@ export default function Experience({experienceData}) {
             {experienceData.slice(0).reverse().map(experience => (
                 <section className="experience mb-12 print:mb-6"
                          key={experience.id}>
-                    <h3 className="mt-2 font-serif font-bold text-[21px] leading-none">{experience.company}</h3>
-                    <p className="mt-2">
+                    <h3 className="mt-2 font-serif font-bold text-[21px] leading-none print:mt-1 print:text-base">{experience.company}</h3>
+                    <p className="mt-2 print:mt-1">
                         <Datetime dateString={experience.date_from}/>
                         {' '}&middot;{' '}
                         <Datetime dateString={experience.date_to}/>
                     </p>
-                    <h4 className="mt-2 font-serif font-bold italic text-[21px]">
+                    <h4 className="mt-2 font-serif font-bold italic text-[21px] print:mt-1 print:text-base">
                         {experience.jobTitle}
                     </h4>
                     <div className="markdown"
--- components/projects.js
+++ components/projects.js
@@ -5,7 +5,7 @@ export default function Projects({projectData}) {
             {projectData.map(project => (
                 <section className="project mb-12 print:mb-6"
                          key={project.id}>
-                    <h3 className="mt-2 font-serif font-bold text-[21px] leading-none">
+                    <h3 className="mt-2 font-serif font-bold text-[21px] leading-none print:mt-1 print:text-base">
                         <a href={project.href} rel="noopener" target="_blank">{project.name}</a>
                     </h3>
                     <div className="markdown"
