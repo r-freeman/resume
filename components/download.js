c3a208a82ce79031023308e8c1f940b32700fa7b
Ryan Freeman <N00173767@student.iadt.ie>
2021-11-03 21:28:41 +0000

    Replaced <Link> with <a> tag in DownloadButton component

--- components/download.js
+++ components/download.js
@@ -1,16 +1,15 @@
 import {DownloadIcon} from './svg';
-import Link from 'next/link';
 
 export default function DownloadButton({author}) {
     return (
-        <>
-            <Link href={encodeURIComponent(`/${author.name} ${author.jobTitle} ${author.location}.pdf`)}>
-                <button className="p-2 text-gray-500 hover:text-gray-900 border rounded-lg focus:ring-2 print:hidden"
-                        aria-label="Download resume">
-                    <DownloadIcon className="w-6 lg:w-5 h-auto"/>
-                    <span className="sr-only">Download resume as PDF</span>
-                </button>
-            </Link>
-        </>
+        <a href={encodeURIComponent(`/${author.name} ${author.jobTitle} ${author.location}.pdf`)}
+           rel="noopener"
+           target="_blank">
+            <button className="p-2 text-gray-500 hover:text-gray-900 border rounded-lg focus:ring-2 print:hidden"
+                    aria-label="Download resume">
+                <DownloadIcon className="w-6 lg:w-5 h-auto"/>
+                <span className="sr-only">Download resume as PDF</span>
+            </button>
+        </a>
     )
 };
